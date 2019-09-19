cbb3644ef1799b3edc444f4dd27b279fde441d7f
JoeLorenzo <Joselmedrano@gmail.com>
2019-09-18 23:50:47 -0500

    -WelcomePage component rendered.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage"
 
 
 export default function App() {
@@ -8,6 +10,9 @@ export default function App() {
     <main>
       <Header />
       {/* <TabNav /> */}
+      <WelcomePage />
+      <Route exact path="/" component="WelcomePage"/>
+
     </main>
   );
 }
